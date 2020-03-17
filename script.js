1a071bf8b906eab98df5c9e7379c2c9c93761ab6
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-03-16 20:53:58 -0700

    adding pseudocode to index.html and script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+// Build a timed code quiz with multiple-choice questions. 
+// This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. 
+// It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+// ```
+// GIVEN I am taking a code quiz
+
+// WHEN I click the start button
+// There must be a start page 
+// There must be start button that prompts the begining of the game 
+
+// THEN a timer starts and I am presented with a question
+// as soon as game starts,
+// time commeces count down
+// first question is presented
+// first question must have a question field
+// first question must have an answer field
+
+
+// WHEN I answer a question
+// THEN I am presented with another question
+// after an answer is submited, 
+// first question is replaced by the next question in line
+// first answer is replaced by the next answer in line 
+// second question must have an answer field 
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// when answer to any question is false, timer value must be adjusted accordingly
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// two ways for the game to end.
+// Timer reaches 0 or player answers all the questions 
+
+// WHEN the game is over
+// THEN I can save my initials and score
+// end game results in the submission of initial and the display of score gained. 
